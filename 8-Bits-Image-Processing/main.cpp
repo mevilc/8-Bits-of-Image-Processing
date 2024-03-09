7de475848cdd08d41cc5124e418f2d7bef42e131
mevilc <crastamevil6@gmail.com>
2024-03-09 12:19:27 -0500

    added motion detection algorithm

--- 8-Bits-Image-Processing/main.cpp
+++ 8-Bits-Image-Processing/main.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 using namespace std;
 
-static void threshold(cv::Mat& img, int threshold)
+static void threshold(cv::Mat& img, float threshold)
 {
-    // 32fp image
+    // [ 0.0, 1.0 ] --> [ 0, 255 ] 
+    int thresholdInt = static_cast< int >(threshold * 255.f);
     for (size_t y = 0; y < img.rows; y++)
         for (size_t x = 0; x < img.cols; x++)
-            img.at< uint8_t >(y, x) = img.at< uint8_t >(y, x) >= threshold ? 255 : 0;
+            img.at< uint8_t >(y, x) = img.at< uint8_t >(y, x) >= thresholdInt ? 255 : 0;
 
 }
 
-enum algorithm { THRESHOLD = 1 };
+static void motionDetect(cv::Mat& fr1, cv::Mat& fr2)
+{
+    // 8bit, 1 channel
+    for (size_t y = 0; y < fr1.rows; y++)
+        for (size_t x = 0; x < fr1.cols; x++)
+            fr2.at< uint8_t >(y, x) = abs( fr2.at< uint8_t >(y, x) - fr1.at< uint8_t >(y, x) );
+}
+
+enum algorithm { THRESHOLD = 1, MOTION_DETECTION };
 
 int main()
 {
@@ -24,35 +34,73 @@ int main()
         return -1;
     }
 
-    cv::Mat frame, frameT;
-    int thresholdVal = 128;
+    cv::Mat unmodFrame, currFrame;
+    
     int algoChoice{};
 
     cout << "Choose algorithm: \n" <<
-        "1. Threshold";
+        "1. Threshold\n" <<
+        "2. Motion Detection\n";
+
     cin >> algoChoice;
 
-    while (1) {
+    // motion detection
+    int n{ 0 };
+    cv::Mat prevFrame;
 
+    while (1) 
+    {
         // Capture frame-by-frame
-        cap >> frame;
+        cap >> unmodFrame;
 
-        // If the frame is empty, break immediately
-        if (frame.empty())
-            break;
+        if (algoChoice == algorithm::MOTION_DETECTION)
+        {
+            if (n % 2 == 0)
+            {
+                prevFrame = unmodFrame.clone();
+                cv::cvtColor(prevFrame, prevFrame, cv::COLOR_BGR2GRAY);
+            }
 
-        cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
+            currFrame = unmodFrame.clone();
 
-        frameT = frame.clone();
+            // If the frame is empty, break immediately
+            if (unmodFrame.empty())
+                break;
 
-        if(algoChoice == algorithm::THRESHOLD) threshold(frameT, thresholdVal);
+            cv::cvtColor(unmodFrame, unmodFrame, cv::COLOR_BGR2GRAY);
+            cv::cvtColor(currFrame, currFrame, cv::COLOR_BGR2GRAY);
 
-        // Display the resulting frame
-        cv::imshow("Frame", frame);
-        cv::imshow("FrameT", frameT);
+            motionDetect(prevFrame, currFrame);
+
+            // Display the resulting frame
+            cv::imshow("Frame", unmodFrame);
+            cv::imshow("Motion", currFrame);
+
+            n++;
+
+        }
+
+        else if ( algoChoice == algorithm::THRESHOLD )
+        {
+            // If the frame is empty, break immediately
+            if (unmodFrame.empty())
+                break;
+
+            currFrame = unmodFrame.clone();
+            cv::cvtColor(unmodFrame, unmodFrame, cv::COLOR_BGR2GRAY);
+            cv::cvtColor(currFrame, currFrame, cv::COLOR_BGR2GRAY);
+
+            float thresholdVal = 0.2f;
+            threshold(currFrame, thresholdVal);
+
+            // Display the resulting frame
+            cv::imshow("Frame", unmodFrame);
+            cv::imshow("Threshold", currFrame);
+        }
+        
 
         // Press ESC on keyboard to exit
-        char c = static_cast< char >( cv::waitKey( 25 ) );
+        char c = static_cast< char >( cv::waitKey( 5 ) );
         if (c == 27)
             break;
     }
